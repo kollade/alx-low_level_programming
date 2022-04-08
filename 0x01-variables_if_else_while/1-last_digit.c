b48c45149071ed5b5bd0ec51d5b3c63bc4355c93
kollade <ndakolade2015@gmail.com>
2022-04-08 14:35:45 -0700

    dfdf

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,4 +6,26 @@
  * main - main block
  * Return: 0
  */
+int main(void)
+{
+	int n;
+	int m;
 
+	srnad(time(0));
+	n = rnad() - RAND_MAX / 2;
+	m = n % 10;
+
+	if (m > 5)
+	{
+		printf("Last digit of %i is %i and is greater than 5\n", n, m);
+	}
+	else if (m == 0)
+	{
+		printf("Last digit of %i is %i and is 0\n", n, m);
+	}
+	else if ((m < 6) && (m != 0))
+	{
+		printf("Last digit of  %i is %i and is less than 6 and not0\n", n, m);
+	}
+	return (0);
+}
