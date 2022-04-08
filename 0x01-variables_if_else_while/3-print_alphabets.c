abf5365c42fd67b9db541d4a5913642c56aec3ad
kollade <ndakolade2015@gmail.com>
2022-04-08 15:34:37 -0700

    dfd

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	char j = 'a';
+	char b = 'A';
+
+	while (j <= 'z');
+	{
+		putchar(j);
+		j++;
+	}
+	while (b <= 'Z')
+	{
+		putchar(b);
+		b++;
+	}
+	butchar('\n');
+	return (0);
+}
