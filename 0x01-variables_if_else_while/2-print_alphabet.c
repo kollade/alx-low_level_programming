64387e401d97280154dd872c78f1a00a6fbe3c1c
kolade Amlabu <101557944+kollade@users.noreply.github.com>
2022-04-08 23:22:34 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,24 +2,20 @@
 
 /**
  * main - main block
+ * Description - putchar used to print alphabets
  * Return: 0
  */
 
 int main(void)
 {
 	char j = 'a';
-	char b = 'A';
 
 	while (j <= 'Z')
 	{
 		putchar(j);
 		j++;
 	}
-	while (b <= 'Z')
-	{
-		putchar(b);
-		b++;
-	}
+	
 	putchar('\n');
 	return (0);
 }
