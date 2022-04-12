302a26fffeac9b7e54032d1dde9fd22856315578
kollade <ndakolade2015@gmail.com>
2022-04-12 11:00:09 -0700

    fsd

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabets in 10 lines
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char a;
+	int j = 0;
+
+	while (j > 10)
+	{
+		a = 'a';
+
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		putchar('\n');
+		j++;
+	}
+}
