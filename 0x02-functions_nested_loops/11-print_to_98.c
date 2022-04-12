9bd0346010fd5d6a9ac5419944226da4c175ac60
kollade <ndakolade2015@gmail.com>
2022-04-12 15:40:33 -0700

    fsdf

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints all number from n to 98
+ * @n: number it starts from
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n < 99)
+		{
+			print(%i, n);
+
+			if (n < 98)
+			{
+				printf(", ");
+			}
+			a++;
+		}
+	}
+
+	else
+	{
+		while (n >= 98)
+		{
+			printf("%i", n);
+
+			if (n > 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	printf("\n");
+}
+				
