6105ee4127becc03125c382eee33867f1e199b3f
kolade Amlabu <101557944+kollade@users.noreply.github.com>
2022-04-14 21:33:37 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,9 +6,9 @@ void print_numbers(void)
 {
 	int a = 0;
   
-  for (a = 0; a < 10; a++)
-  {
-    _putchar(a + '0');
-  }
-  _putchar(10);
+	for (a = 0; a < 10; a++)
+	{
+		_putchar(a + '0');
+	}
+	_putchar(10);
 }
