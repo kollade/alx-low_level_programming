fbf430060db6491557cea064ca86085a98e9c215
kolade Amlabu <101557944+kollade@users.noreply.github.com>
2022-04-14 14:59:43 +0100

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,2 +1,29 @@
-#include "main.h"
-
+#include "main.h"                                                                                                                                        
+                                                                                                                                                         
+/**                                                                                                                                                      
+ * largest_number - return the largest of 3 numbers                                                                                                      
+ * @a: first integer                                                                                                                                     
+ * @b: second integer                                                                                                                                    
+ * @c: third integer                                                                                                                                     
+ * Return: largest number                                                                                                                                
+ */                                                                                                                                                      
+                                                                                                                                                         
+int largest_numbers(int a, int b, int c)                                                                                                                 
+{                                                                                                                                                        
+        int largest;                                                                                                                                     
+                                                                                                                                                         
+        if (a >= b && a >= c)                                                                                                                            
+        {                                                                                                                                                
+                largest = a;                                                                                                                             
+        }                                                                                                                                                
+        if  (b >= a && b >= c)                                                                                                                           
+        {                                                                                                                                                
+                largest = b;                                                                                                                             
+        }                                                                                                                                                
+        if (c >= a && c >= b)                                                                                                                            
+        {                                                                                                                                                
+                largest = c;                                                                                                                             
+        }                                                                                                                                                
+                                                                                                                                                         
+        return (largest);                                                                                                                                
+}                                    
