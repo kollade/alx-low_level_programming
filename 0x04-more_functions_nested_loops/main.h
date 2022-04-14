310ec344cf03f8454d2565fd45570053ac3f9713
kolade Amlabu <101557944+kollade@users.noreply.github.com>
2022-04-14 22:26:39 +0100

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,6 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-int convert_day(int month, int day);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -9,4 +8,3 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-
