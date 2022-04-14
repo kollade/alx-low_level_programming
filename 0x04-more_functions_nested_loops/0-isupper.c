86ded803274d6315776e0359a0f53e59a9700290
kolade Amlabu <101557944+kollade@users.noreply.github.com>
2022-04-14 21:03:49 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,9 +4,9 @@
  * @c: character to be checked
  * Return: 1 if uppercase, 0 otherwise
  */
-int _isupper(int c )
+int _isupper(int c)
 {
-	if (c >= 65 && c <=90)
+	if (c >= 65 && c <= 90)
 		return (1);
 	else
 		return (0);
