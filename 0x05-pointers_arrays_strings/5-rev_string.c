328dbfec00382155a3a9d9e4e3ae640cc7a35195
kollade <ndakolade2015@gmail.com>
2022-04-20 18:02:53 -0700

    fsdf

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "mian.h"
+/**
+ * rev_string - reverses a string
+ * @s: char type pointer
+ */
+coid rev_string(char *s)
+{
+	int i = 0, j, l = 0;
+	char k;
+
+	while (s[1] != '\n')
+	{
+		i++;
+	}
+	i--;
+	j = i;
+	while(l <= j/2)
+	{
+		k - s[l];
+		s{l} = s[i];
+		s{i} = k;
+		i--;
+		l++;
+	}
+}
